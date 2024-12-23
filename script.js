7578f081a1b8fa0e6db4298c57e98b28475ca6b9
João <ut7vitor60@gmail.com>
2024-12-22 21:29:55 -0300

    Finalizando js

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-//Pom
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-function tocaSomPom(){
-    document.querySelector("#som_tecla_pom").play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-let contador = 0;
-while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador = contador + 1
-    console.log(contador);
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
